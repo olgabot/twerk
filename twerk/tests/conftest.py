d9072f67493218732ed53acf02e9d62f1e3815cf
Olga Botvinnik <olga.botvinnik@gmail.com>
2015-05-21 10:10:23 -0700

    fix test dataframe

--- twerk/tests/conftest.py
+++ twerk/tests/conftest.py
@@ -9,11 +9,11 @@ def X():
     nrow = 20
     ncol = 10
     data = np.random.randn(nrow*ncol).reshape(nrow, ncol)
-    index = string.ascii_lowercase[:nrow]
-    columns = string.ascii_uppercase[:ncol]
+    index = list(string.ascii_lowercase[:nrow])
+    columns = list(string.ascii_uppercase[:ncol])
     data = pd.DataFrame(data, index=index, columns=columns)
-    data.ix[:nrow/2, :ncol/2] += 5
-    data.ix[nrow/2:, ncol/2:] -= 5
+    data.ix[:nrow/2, :ncol/2] += 1
+    data.ix[:nrow/2, ncol/2:] -= 1
     return data
 
 @pytest.fixture(params=[None, 'spearman', 'pearson', 'kendall', 'bicor'])
